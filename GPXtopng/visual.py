6955c1235262ca0168e7821f273c62b71ed939bb
Jakob <jajoha@outlook.at>
2023-12-19 19:34:43 +0100

    bounds works, markers are worse

--- GPXtopng/visual.py
+++ GPXtopng/visual.py
@@ -4,8 +4,9 @@ import folium
 from folium import plugins
 
 def plot_gpx_folder(folder_path, map_filename="map.html"):
-    bound = [[54, 88], [58, 15]]
-    m = folium.Map(location=[0, 0], zoom_start=10)  # Initialize the map
+    bounds = [[54, 18], [58, 1]]
+    m = folium.Map(location=[56, 10], zoom_start=7, max_bounds=True, min_lat=bounds[0][0], max_lat=bounds[1][0], min_lon=bounds[0][1], max_lon=bounds[1][1])
+    marker_cluster = plugins.MarkerCluster().add_to(m)
     total_points = 0
     total_lat = 0
     total_lon = 0
@@ -13,24 +14,20 @@ def plot_gpx_folder(folder_path, map_filename="map.html"):
     for filename in os.listdir(folder_path):
         if filename.endswith('.gpx'):
             gpx_file_path = os.path.join(folder_path, filename)
-            total_lat, total_lon, total_points = plot_gpx_file(gpx_file_path, m, total_lat, total_lon, total_points)
+            marker_cluster, total_lat, total_lon, total_points = plot_gpx_file(gpx_file_path, m, marker_cluster, total_lat, total_lon, total_points)
 
     if total_points > 0:
         average_lat = total_lat / total_points
         average_lon = total_lon / total_points
-        m = folium.Map(location=[average_lat, average_lon], zoom_start=12)
-
-        for filename in os.listdir(folder_path):
-            if filename.endswith('.gpx'):
-                gpx_file_path = os.path.join(folder_path, filename)
-                plot_gpx_file(gpx_file_path, m, total_lat, total_lon, total_points)
+        m.location = [average_lat, average_lon]  # Set the new center for the map
+        m.zoom_start = 12  # Set the new zoom level
 
         m.save(map_filename)
         print(f"Map saved as {map_filename}")
     else:
         print("No valid coordinates found in GPX files.")
 
-def plot_gpx_file(gpx_file_path, map_obj, total_lat, total_lon, total_points):
+def plot_gpx_file(gpx_file_path, map_obj, marker_cluster, total_lat, total_lon, total_points):
     with open(gpx_file_path, 'r') as gpx_file:
         gpx = gpxpy.parse(gpx_file)
         for track in gpx.tracks:
@@ -40,13 +37,15 @@ def plot_gpx_file(gpx_file_path, map_obj, total_lat, total_lon, total_points):
                 total_lat += sum(lats)
                 total_lon += sum(lons)
                 total_points += len(lats)
-                plot_points_on_map(lats, lons, map_obj)
+                marker_cluster = plot_points_on_map(lats, lons, map_obj, marker_cluster)
 
-    return total_lat, total_lon, total_points
+    return marker_cluster, total_lat, total_lon, total_points
 
-def plot_points_on_map(lats, lons, map_obj):
+def plot_points_on_map(lats, lons, map_obj, marker_cluster):
     for lat, lon in zip(lats, lons):
-        folium.CircleMarker(location=[lat, lon], radius=3, color='blue').add_to(map_obj)
+        folium.Marker(location=[lat, lon]).add_to(marker_cluster)
+
+    return marker_cluster
 
 if __name__ == "__main__":
     folder_path = "/Users/jakobsmac/Public/python_script/source"
